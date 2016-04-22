856e396494fa60e3850f9315db9f3c2443b8fb31
Maciej Stanik <maciej.stanik@tapptic.com>
2016-04-22 20:33:57 +0200

    Notifications

--- FruitsDiet/AppDelegate.swift
+++ FruitsDiet/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil))
         IQKeyboardManager.sharedManager().enable = true
         IQKeyboardManager.sharedManager().enableAutoToolbar = false
         IQKeyboardManager.sharedManager().shouldResignOnTouchOutside = true
---
+++ FruitsDiet/Notifications.swift
@@ -0,0 +1,26 @@
+//
+//  Notifications.swift
+//  FruitsDiet
+//
+//  Created by Maciej Stanik on 22/04/16.
+//  Copyright © 2016 Maciej Stanik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Notifications {
+    
+    class func display(text: String){
+        
+        let notification: UILocalNotification = UILocalNotification()
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        
+        let dateTime = NSDate()
+        notification.fireDate = dateTime
+        notification.alertBody = text
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+    }
+    
+}
